41f50d7ad01bee4d2c7c2b2059f1933fd1a2c335
Daniel Pritsker <danielpr137@gmail.com>
2021-12-14 19:15:27 +0200

    Added candidate, job and skill models

--- basic_matcher/candidate_finder/models.py
+++ basic_matcher/candidate_finder/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Skill(models.Model):
+    """Skill model for the Jobs and the Candidates"""
+    name = models.CharField(max_length=40)
+    def __str__(self):
+        """Return string representation of the skill"""
+        return self.name
+
+
+class Candidate(models.Model):
+    """Database model for Candidates in the system"""
+    name = models.CharField(max_length=60) 
+    title = models.CharField(max_length=255)
+    skills = models.ManyToManyField(Skill)
+
+    def get_full_name(self):
+        """Retrieve full name of candidate"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of the candidate"""
+        return self.name
+
+
+class Job(models.Model):
+    """Database model for Jobs in the system"""
+
+    title = models.CharField(max_length=60)
+    skills = models.ManyToManyField(Skill)
+
+    def __str__(self):
+        return self.title
+
