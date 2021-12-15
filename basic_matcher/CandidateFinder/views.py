acd0ff59676360e2ec8463645f7b03f7fe50721f
Daniel Pritsker <danielpr137@gmail.com>
2021-12-15 11:23:33 +0200

    Added candidate finder function to views

--- basic_matcher/CandidateFinder/views.py
+++ basic_matcher/CandidateFinder/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.db.models import Count
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import *
+
+def find_candidate(request, job_title):
+    """Find the best candidate for a job"""
+    matching_title_candidates = Candidate.objects.filter(title=job_title)
+    job_skills = Job.object.get(title = job_title).skills.all()
+    print(job_skills)
+    matching_candidates = matching_title_candidates.filter(skills__in=job_skills)
+    print(matching_candidates)
+    sorted_matching_candidates = matching_candidates.annotate(count = Count('name')).order_by('-count')
+    print(sorted_matching_candidates)
+
+    return HttpResponse(f'List of skills = {queryset_to_str(job_skills)}')
+
+def queryset_to_str(queryset):
+    arr = []
+    for query in queryset:
+        arr.append(str(query))
+    return arr
--- basic_matcher/basic_matcher/settings.py
+++ basic_matcher/basic_matcher/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #'rest_framework',
-    #'CandidateFinder',  
-    'CandidateFinder.apps.CandidateFinderConfig',
+    'CandidateFinder',  
+    #'CandidateFinder.apps.CandidateFinderConfig',
 ]
 
 MIDDLEWARE = [
